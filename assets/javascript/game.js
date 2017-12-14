ab6b15611ee8c1288978f0de2905f1cdef72490b
Jeremy Gruhlkey <jeremygruhlkey@Naeuns-MacBook-Pro.local>
2017-12-13 17:01:24 -0800

    Function chooseDefender working
    Currently works if luke selected as character
    Needs code for other four characters
    Changed .slected to .enemy

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,58 +1,125 @@
 // each characters fight data stored in an object
 $(document).ready(function(){
 
-var lukeChar = {
-    health: 125,
-    attack: 7,
-    counterAttack: 25
-};
-
-var hanChar = {
-    health: 100,
-    attack: 7,
-    counterAttack: 15
-};
-
-var bobaChar = {
-    health: 115,
-    attack: 7,
-    counterAttack: 30
-};
-
-var vaderChar = {
-    health: 140,
-    attack: 7,
-    counterAttack: 35
-};
-
-playerOne = 0;
-opponent = 0;
-
-$(".choice").click(function() {
-    var selection = $(this);
-    if (selection.hasClass("luke")) {
-        playerOne = luke;
-        $("#han").addClass("selected");
-        $("#enemies-available").append($("#han"));
-        $("#boba").addClass("selected");
-        $("#enemies-available").append($("#boba"));
-        $("#vader").addClass("selected");
-        $("#enemies-available").append($("#vader"));
-        console.log(playerOne)
-
-    //     // rewrite other three character divs to .enemies-avaialable
-    //     // remove form/hide within .character-choices
-    //     // addClass for red background to other three characters
-   
+    var lukeChar = {
+        health: 120,
+        attack: 7,
+        counterAttack: 25
     };
 
-});
+    var hanChar = {
+        health: 100,
+        attack: 7,
+        counterAttack: 15
+    };
+
+    var bobaChar = {
+        health: 110,
+        attack: 7,
+        counterAttack: 30
+    };
+
+    var vaderChar = {
+        health: 130,
+        attack: 7,
+        counterAttack: 35
+    };
+
+    var playerOne;
+    var defender;
+
+    function choosePlayer() {
+        $(".choice").click(function() {
+            var selection = $(this);
+
+            if (playerOne) {
+                chooseDefender();
+                console.log(playerOne);
+                return;
+            }
+
+            
+            if (selection.hasClass("luke")) {
+                playerOne = lukeChar;
+                $("#han").addClass("enemy");
+                $("#enemies-available").append($("#han"));
+                $("#boba").addClass("enemy");
+                $("#enemies-available").append($("#boba"));
+                $("#vader").addClass("enemy");
+                $("#enemies-available").append($("#vader"));
+                console.log(playerOne);
+            }
+
+            else if (selection.hasClass("han")) {
+                playerOne = hanChar;
+                $("#luke").addClass("enemy");
+                // $("#luke").removeClass("choice");
+                $("#enemies-available").append($("#luke"));
+                $("#boba").addClass("enemy");
+                $("#enemies-available").append($("#boba"));
+                $("#vader").addClass("enemy");
+                $("#enemies-available").append($("#vader"));
+                console.log(playerOne);
+            }
+
+            else if (selection.hasClass("boba")) {
+                playerOne = bobaChar;
+                $("#luke").addClass("enemy");
+                $("#enemies-available").append($("#luke"));
+                $("#han").addClass("enemy");
+                $("#enemies-available").append($("#han"));
+                $("#vader").addClass("enemy");
+                $("#enemies-available").append($("#vader"));
+                console.log(playerOne);
+            }
+
+            else if (selection.hasClass("vader")) {
+                playerOne = bobaChar;
+                $("#luke").addClass("enemy");
+                $("#enemies-available").append($("#luke"));
+                $("#han").addClass("enemy");
+                $("#enemies-available").append($("#han"));
+                $("#boba").addClass("enemy");
+                $("#enemies-available").append($("#boba"));
+                console.log(playerOne);
+            }
+            
+            chooseDefender();
     
+    
+        });
+    };
+
+    function chooseDefender() {
         
+        if (defender) {
+            console.log(defender);
+            return;
+        }
+
+        $(".enemy").click(function() {
+            var enemySelection = $(this);
+
+            if (enemySelection.hasClass("luke")) {
+                defender = lukeChar;
+                $(".defender-box").append($("#luke"));
+                console.log(defender);
+            }
+
+        });
+
+    };
+
     
-    
+    choosePlayer();  
+
     
 });
+    
+  
+
+    
+
 
 
 
