2abd1179f55b4d1f88ce4f00632a0777353439b9
Jeremy Gruhlkey <jeremygruhlkey@Naeuns-MacBook-Pro.local>
2017-12-16 14:55:40 -0800

    Final pre submission push with touch ups
    cleaned up code, removing spaces, console.logs
    added sound effects
    removed unused variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,38 @@
-// each characters fight data stored in an object
 $(document).ready(function(){
 
     var lukeChar = {
         health: 135,
-        attack: 7,
         counterAttack: 25
     };
 
     var hanChar = {
-        health: 100,
-        attack: 7,
+        health: 100,        
         counterAttack: 20
     };
 
     var bobaChar = {
-        health: 120,
-        attack: 7,
+        health: 150,        
         counterAttack: 30
     };
 
     var vaderChar = {
-        health: 190,
-        attack: 7,
+        health: 230,        
         counterAttack: 30
     };
 
     var playerOne;
     var defender;
     var attack = 5;
-
     var playerOneHealthDiv;
     var gameOver = false;
     var winCounter = 0;
+    var forAttack = new Audio ("http://www.thesoundarchive.com/starwars/blaster-firing.mp3");
+    var forBoba = new Audio ("http://www.thesoundarchive.com/starwars/set-for-stun.mp3");
+    var forLuke = new Audio ("http://www.thesoundarchive.com/starwars/light-saber-on.mp3");
+    var forVader = new Audio ("http://www.thesoundarchive.com/starwars/swvader02.mp3");
+    var forHan = new Audio ("http://www.thesoundarchive.com/starwars/empire/laughfuzzball.mp3");
+    var forLoss = new Audio ("http://www.thesoundarchive.com/starwars/WilhelmScream.mp3");
+    var forWin = new Audio ("http://www.thesoundarchive.com/starwars/force.mp3");
     
 
     function choosePlayer() {
@@ -46,111 +47,89 @@ $(document).ready(function(){
             else if (selection.hasClass("luke")) {
                 playerOne = lukeChar;
                 playerOneHealthDiv = $("#luke-health");
-                $("#han").addClass("enemy");
-                // $("#han").removeClass("choice");
-                // $("#han").removeClass("han");
+                forLuke.play();
+                $("#han").addClass("enemy");                
                 $("#enemies-available").append($("#han"));
                 $("#boba").addClass("enemy");
                 $("#enemies-available").append($("#boba"));
                 $("#vader").addClass("enemy");
-                $("#enemies-available").append($("#vader"));
-                // console.log(playerOne);
-                // console.log(playerOneHealthDiv);
-                chooseDefender();
-                
+                $("#enemies-available").append($("#vader"));                
             }
 
             else if (selection.hasClass("han")) {
                 playerOne = hanChar;
                 playerOneHealthDiv = $("#han-health");
-                $("#luke").addClass("enemy");
-                // $("#luke").removeClass("choice");
+                forHan.play();
+                $("#luke").addClass("enemy");              
                 $("#enemies-available").append($("#luke"));
                 $("#boba").addClass("enemy");
                 $("#enemies-available").append($("#boba"));
                 $("#vader").addClass("enemy");
-                $("#enemies-available").append($("#vader"));
-                // console.log(playerOne);
-                // console.log(playerOneHealthDiv);
-                chooseDefender();
-                
+                $("#enemies-available").append($("#vader"));              
             }
 
             else if (selection.hasClass("boba")) {
                 playerOne = bobaChar;
                 playerOneHealthDiv = $("#boba-health");
+                forBoba.play();
                 $("#luke").addClass("enemy");
                 $("#enemies-available").append($("#luke"));
                 $("#han").addClass("enemy");
                 $("#enemies-available").append($("#han"));
                 $("#vader").addClass("enemy");
                 $("#enemies-available").append($("#vader"));
-                // console.log(playerOne);
-                // console.log(playerOneHealthDiv);
-                chooseDefender();
-                
-            }
+           }
 
             else if (selection.hasClass("vader")) {
                 playerOne = bobaChar;
                 playerOneHealthDiv = $("#vader-health");
+                forVader.play();
                 $("#luke").addClass("enemy");
                 $("#enemies-available").append($("#luke"));
                 $("#han").addClass("enemy");
                 $("#enemies-available").append($("#han"));
                 $("#boba").addClass("enemy");
-                $("#enemies-available").append($("#boba"));
-                // console.log(playerOne);
-                // console.log(playerOneHealthDiv);
-                chooseDefender();
-                
-            }
-            // $('.choice').off('click')
-    
+                $("#enemies-available").append($("#boba"));              
+            }    
+            chooseDefender();
         });
     };
 
     function chooseDefender() {
-        
-        // if (defender) {
-        //     doBattle();
-        //     return;
-        // }
-        $('.enemy').on('click');
-        
+        $('.enemy').on('click');        
         $(".enemy").click(function() {
             var enemySelection = $(this);
 
             if (enemySelection.hasClass("luke")) {
                 defender = lukeChar;
                 defenderHealthDiv = $("#luke-health");
+                forLuke.play();
                 $("#luke").addClass("defender-black");
-                $(".defender-box").append($("#luke"));
-                // console.log(defender);
+                $(".defender-box").append($("#luke"));    
             }
 
             else if (enemySelection.hasClass("han")) {
                 defender = hanChar;
                 defenderHealthDiv = $("#han-health");
+                forHan.play();
                 $("#han").addClass("defender-black");
-                $(".defender-box").append($("#han"));
-                // console.log(defender);
+                $(".defender-box").append($("#han"));           
             }
 
             else if (enemySelection.hasClass("boba")) {
                 defender = bobaChar;
                 defenderHealthDiv = $("#boba-health");
+                forBoba.play();
                 $("#boba").addClass("defender-black");                
-                $(".defender-box").append($("#boba"));
-                // console.log(defender);
+                $(".defender-box").append($("#boba"));               
             }
 
             else if (enemySelection.hasClass("vader")) {
                 defender = vaderChar;
                 defenderHealthDiv = $("#vader-health");
+                forVader.play();
                 $("#vader").addClass("defender-black");                
-                $(".defender-box").append($("#vader"));
-                // console.log(defender);
+                $(".defender-box").append($("#vader"));               
             }
 
             $('.enemy').off('click');
@@ -163,40 +142,35 @@ $(document).ready(function(){
         if (gameOver) {
             restart();
         }
-        // var attack = 5;
-        var playerOneHealth = playerOne.health;
-            // console.log("player one health: " + playerOneHealth);
+    
+        var playerOneHealth = playerOne.health;            
         var defenderHealth = defender.health;
         var defenderAttack = defender.counterAttack;
-            // console.log("defender health: " + defenderHealth);
             $("#attack").click(function() {
+                forAttack.play();
                 playerOneHealth = playerOneHealth - defenderAttack;
                 defenderHealth = defenderHealth - attack;
-                    // console.log("new playerOne health: " + playerOneHealth);
-                    // console.log("new defender health: " + defenderHealth);
-                    
                 $(playerOneHealthDiv).html(playerOneHealth);
                 $(defenderHealthDiv).html(defenderHealth);
                 var update = "You hit for " + attack + " and were hit back for " + defenderAttack;
                 $(".fight-update").html(update);
                 attack = attack + 5;
-                // console.log("new attack: " + attack);
+                
                     if (playerOneHealth <= 0 && defenderHealth > 0) {
                         gameOver = true;
+                        forLoss.play();
                         $(".fight-update").html("You LOST!!! Click restart to play again.");
                         $("#attack").off("click");
                         restart();
                     }
-                    // else if ( $(".enemies-available").is(":empty") && $(".defender-box").is(":empty") ){
-                    //     $("fight-update").html("You WON!!!! Click restart to play again!!");
-                    //     $(".restart").addClass("unhide-button");
-                    // }
+                    
                     else if (defenderHealth <=  0 && playerOneHealth > 0) {
                         winCounter = winCounter + 1;
-                        console.log("win counter: " + winCounter);
+                        // console.log("win counter: " + winCounter);
                         $(".defender-box").empty();
                             if (winCounter > 2){
                                 $(".fight-update").html("You WIN!! The galaxy is yours. RESTART to play again!")
+                                setTimeout(forWinFunc, 1000);
                                 restart();
                             }
                             else {
@@ -206,18 +180,9 @@ $(document).ready(function(){
                         $('#attack').off('click');
                         $('.enemy').on('click');
                         
-                        
                         chooseDefender();
-                    }
-                    
-                    
-        
-                    
-
-
-
+                    } 
                 });
-
     };
 
     function restart() {
@@ -227,13 +192,11 @@ $(document).ready(function(){
         })
     };
 
-    // function theEnd() {
-    //     $("fight-update").html("You WON!!!! Click restart to play again!!");
-    //     $(".restart").addClass("unhide-button");
-    // };
+    function forWinFunc() {
+        forWin.play();
+    };
 
     choosePlayer();  
-    
     
 });
     
